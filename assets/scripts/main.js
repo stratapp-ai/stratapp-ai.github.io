e8314624877af7f49731192a2ae150f08fb82417
Nik Nieuwenhuis <niknewys@gmail.com>
2020-06-16 11:03:09 +1000

    features read more

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -95,12 +95,12 @@ $(".ani").inViewport(function(px){
 });
 
 
-$('.shrinkable').readmore({ 
-    speed: 75, 
-    collapsedHeight: 195,
-    lessLink: '<a href="#" class="c-feature__link">read less</a>',
-    moreLink: '<a href="#" class="c-feature__link">read more</a>'
-});
+// $('.shrinkable').readmore({ 
+//     speed: 75, 
+//     collapsedHeight: 195,
+//     lessLink: '<a href="#" class="c-feature__link">read less</a>',
+//     moreLink: '<a href="#" class="c-feature__link">read more</a>'
+// });
  
  /*--------------------------------
   Swap video with autoplay video
